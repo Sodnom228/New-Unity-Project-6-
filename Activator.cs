90f99fc8a0cd60d745190b41093d2c2acd210378
Sodnom228 <150505714+Sodnom228@users.noreply.github.com>
2023-11-11 15:31:10 +0800

    Add files via upload

---
+++ Activator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Activator : MonoBehaviour
+{
+    public GameObject[] firstGroup;
+    public GameObject[] secondGroup;
+    public Activator button;
+    public Material normal;
+    public Material Transparent;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        if (other.CompareTag("button") || other.CompareTag("Player"))
+        {
+            foreach(GameObject first in firstGroup)
+            {
+                first.GetComponent<Renderer>().material = normal;
+                first.GetComponent<Collider>().isTrigger = false;
+            }
+            foreach (GameObject second in secondGroup)
+            {
+                second.GetComponent<Renderer>().material = Transparent;
+                second.GetComponent<Collider>().isTrigger = true;
+            }
+            GetComponent<Renderer>().material = Transparent;
+            button.GetComponent<Renderer>().material = normal;
+
+        }
+    }
+}
